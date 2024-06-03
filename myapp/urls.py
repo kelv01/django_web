31c46f748ee8a63d1ce93838d8b9800377db4bb1
kelv01 <kelvinnjugunanungari@gmail.com>
2024-06-03 23:11:37 +0300

    added login page and order page

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('order/', views.order, name='order'),
 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 def main (request):
     return render(request, 'main.html')
+
+def about (request):
+    return render(request, 'about.html')
+
+def login (request):
+    return render(request, 'login.html')
+
+def order (request):
+    return render(request, 'order.html')
+
+def contact (request):
+    return render(request, 'contact.html')
+
