25cfb95e7ad9c669383ce4b76523d3cf08c80951
kelv01 <kelvinnjugunanungari@gmail.com>
2024-06-20 21:59:48 +0300

    added responsive for phones

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('login/', views.login, name='login'),
+   # path('login/', views.login, name='login'),
     path('order/', views.order_view, name='order'),
 
 ]
